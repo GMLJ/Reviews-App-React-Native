97526a4efaddd7c161131e28ddf6aaa1ed55762c
G.M <47867339+GMLJ@users.noreply.github.com>
2021-07-29 09:54:54 +0200

    Nav Stack

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
+import "react-native-gesture-handler";
 import React, { useState } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import Home from "./screens/home";
+import ReviewDetails from "./screens/reviewDetails";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="review details" component={ReviewDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+// ReactNavigation:
+// 1 - npm install @react-navigation/native
+// 2 - expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+// => to install dependencies into expo project
+// 3 - npm install @react-navigation/stack
